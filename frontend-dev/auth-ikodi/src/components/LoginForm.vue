ba4f165ace720916a6bb5b159f6e26918e3866ff
ikodi-julien <jupellin39@gmail.com>
2021-10-30 16:16:13 +0200

    Setup placeholder for API requests (test with json-server) -> ok, next API setup

--- frontend-dev/auth-ikodi/src/components/LoginForm.vue
+++ frontend-dev/auth-ikodi/src/components/LoginForm.vue
@@ -1,18 +1,45 @@
 <template>
-  <form @submit.prevent >
-    <input type="text" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Mot de passe">
+  <form @submit.prevent="onSubmit" >
+    <input 
+      type="text" 
+      name="email" 
+      placeholder="Email" 
+      v-model="this.email"
+    >
+    <input 
+      type="password" 
+      name="password" 
+      placeholder="Mot de passe" 
+      v-model="this.password"
+    >
     <Button text="Connexion" className="--blue" width="100%" />
   </form>
 </template>
 
 <script>
 import Button from './Button.vue';
+import axios from 'axios';
 
 export default {
   name: 'LoginForm',
   components: {
     Button,
+  },
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        const response = await axios.get("http://localhost:3000/me");
+        console.log(response.data);
+      } catch(error) {
+        console.log(error);
+      }
+    }
   }
 }
 </script>
--- frontend-dev/auth-ikodi/src/components/SignupForm.vue
+++ frontend-dev/auth-ikodi/src/components/SignupForm.vue
@@ -1,10 +1,35 @@
 <template>
-  <form @submit.prevent >
-    <input type="text" name="firstname" placeholder="Prénom">
-    <input type="text" name="lastname" placeholder="Nom">
-    <input type="email" name="email" placeholder="Email">
-    <input type="password" name="password1" placeholder="Mot de passe">
-    <input type="password" name="password2" placeholder="Confirmation du mot de passe">
+  <form @submit.prevent="onSubmit" >
+    <input 
+      type="text" 
+      name="firstname" 
+      placeholder="Prénom" 
+      v-model="firstname"
+    >
+    <input 
+      type="text" 
+      name="lastname" 
+      placeholder="Nom"
+      v-model="lastname"
+    >
+    <input 
+      type="email" 
+      name="email" 
+      placeholder="Email"
+      v-model="email"
+    >
+    <input 
+      type="password" 
+      name="password1" 
+      placeholder="Mot de passe"
+      v-model="password1"
+    >
+    <input 
+      type="password" 
+      name="password2" 
+      placeholder="Confirmation du mot de passe"
+      v-model="password2"
+    >
     <Button 
       text="Valider" 
       className="--blue" 
@@ -15,11 +40,47 @@
 
 <script>
 import Button from './Button.vue';
+import axios from 'axios';
 
 export default {
   name: 'SignupForm',
   components: {
     Button
+  },
+  data() {
+    return {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password1: "",
+      password2: ""
+    }
+  },
+  methods: {
+    async onSubmit() {
+      
+      if (this.password1 !== this.password2) {
+        alert("Les mots de passe doivent être identiques");
+        return;
+      }
+      
+      const firstname = this.firstname;
+      const lastname = this.lastname;
+      const email = this.email;
+      const password = this.password1;
+      
+      try {
+        await axios.post("http://localhost:3000/new", {
+          firstname,
+          lastname,
+          email,
+          password
+        });
+        
+      } catch(error) {
+      console.log(error);
+      }
+    }
   }
 }
 </script>
