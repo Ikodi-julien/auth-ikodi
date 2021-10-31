3366e4eaf4be6a3ff6768426b7eab216557d9509
ikodi-julien <jupellin39@gmail.com>
2021-10-31 09:44:12 +0100

    Minimal app express running

---
+++ api/ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+    name   : "authServer",
+    script : "npm run startDev",
+    watch: true,
+    out_file: "./output.log"
+  }]
+}
---
+++ api/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const cors = require('cors');
+const PORT = process.env.PORT;
+
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? [/\.ikodi.eu\/?$/] : [`http://localhost:${PORT}`],
+  credentials: true
+}
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log('GET');
+  res.sendStatus(200)
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Started on http://localhost:${PORT}`)
+})
