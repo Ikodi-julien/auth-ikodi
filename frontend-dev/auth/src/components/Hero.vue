a2479a4b9d0ee5d3992951221ba339b9c082ba65
ikodi-julien <jupellin39@gmail.com>
2021-11-05 12:37:08 +0100

    Config component tests, LoginForm.spec.js, rename project to 'auth'

---
+++ frontend-dev/auth/src/components/Hero.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hero">
+    <img src="../assets/auth-hero.png" alt="hero img" class="hero__img" />
+    <div class="hero__text">Se connecter ou créer un compte</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<style scoped lang="scss">
+.hero {
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+  
+  & img {
+    width: 100%;
+    border-radius: 15px;
+  }
+  &__text {
+    width: 90%;
+    padding: 20px 0 20px 0;
+    text-align: center;
+    font-size: 1.5rem;
+  }
+  
+  @media (max-width: 750px) {
+  width: 80%;
+  margin-right: 0;
+  }
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+}
+</style>
---
+++ frontend-dev/auth/src/components/LoginForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit.prevent="onSubmit" >
+    <input
+      type="text"
+      name="email"
+      placeholder="Email"
+      v-model="this.email"
+    >
+    <input
+      type="password"
+      name="password"
+      placeholder="Mot de passe"
+      v-model="this.password"
+    >
+    <Button @click="$emit('submit')" text="Connexion" className="--blue" width="100%" />
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+import Button from './Button.vue';
+
+export default {
+  name: 'LoginForm',
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        const response = await axios.get("http://localhost:3000/me");
+        console.log(response.data);
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  },
+  emits: ['submit']
+
+}
+</script>
+
+<style lang="scss">
+  @import '../styles/form.scss';
+</style>
