964d130e993b1bf5713007e92f31374c8c2c35b6
ikodi-julien <jupellin39@gmail.com>
2021-12-28 17:23:10 +0100

    Fix with password atempt n7

--- api/services/google.tools.js
+++ api/services/google.tools.js
@@ -1,5 +1,9 @@
-const { google } = require('googleapis');
-const OAuth2Data = require('./google_key_local') 
+const { google } = require("googleapis");
+
+const OAuth2Data =
+  process.env.NODE_ENV === "production"
+    ? require("./google_key")
+    : require("./google_key_local");
 // const OAuth2Data = {
 //   web: {
 //     client_id: 1234,
@@ -13,27 +17,26 @@ const OAuth2Data = require('./google_key_local')
 const oAuth2Client = new google.auth.OAuth2(
   OAuth2Data.web.client_id,
   OAuth2Data.web.client_secret,
-  OAuth2Data.web.redirect_uris,
+  OAuth2Data.web.redirect_uris
 );
 
 /**
  * Generates the google account identification's url.
  */
 const googleAuthUri = oAuth2Client.generateAuthUrl({
-  access_type: 'offline',
-  prompt: 'consent',
+  access_type: "offline",
+  prompt: "consent",
   scope: [
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile',
-]
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+  ],
 });
 
 /**
  * Gets the informations sent by google People API
- * @param {string} code 
+ * @param {string} code
  */
 const getGoogleAccountFromCode = async (code) => {
-
   try {
     // Gets the access token for the given code.
     const data = await oAuth2Client.getToken(code);
@@ -44,35 +47,34 @@ const getGoogleAccountFromCode = async (code) => {
 
     // The user agreed to allow our app by giving appropriates tokens.
     // We can now use those tokens with our client to get the corresponding infos from the API
-    const people = google.people({ version: 'v1', auth: oAuth2Client });
+    const people = google.people({ version: "v1", auth: oAuth2Client });
 
     const me = await people.people.get({
       personFields: [`emailAddresses`, `names`],
-      resourceName: 'people/me'
+      resourceName: "people/me",
     });
 
     const firstname = me.data.names[0].givenName;
     const lastname = me.data.names[0].familyName;
     const nickname = `${firstname}-${lastname}`;
     const email = me.data.emailAddresses[0].value;
-    const password = email.substring(3, 9) + '#ikodi-auth!';
+    const password = email.substring(3, 9) + "#ikodi-auth!";
 
     return {
       nickname,
       firstname,
       lastname,
       password,
-      email
+      email,
     };
-
   } catch (error) {
-    console.log(error)
-    return false
+    console.log(error);
+    return false;
   }
-}
+};
 
 module.exports = {
   googleAuthUri,
-  getGoogleAccountFromCode
-}
+  getGoogleAccountFromCode,
+};
 // console.log(url);
