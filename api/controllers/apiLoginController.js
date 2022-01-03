ac179abce9226b94cbb05a33df67027834705bab
ikodi-julien <jupellin39@gmail.com>
2022-01-03 11:26:14 +0100

    not important

--- api/controllers/apiLoginController.js
+++ api/controllers/apiLoginController.js
@@ -1,31 +1,31 @@
-const {googleAuthUri} = require('../services/google.tools');
-const {githubAuthUri} = require('../services/github.tools');
-const {jwtService} = require('../services/jwt.service');
-const cookieService = require('../services/cookie.service');
-const redisService = require('../services/redis.service');
-const queries = require('../queries/authQueries');
-const {URL_CHUNK} = require('../services/settings');
-const bcrypt = require('bcrypt');
-const {redirect} = require('./authController')
+const { googleAuthUri } = require("../services/google.tools");
+const { githubAuthUri } = require("../services/github.tools");
+const { jwtService } = require("../services/jwt.service");
+const cookieService = require("../services/cookie.service");
+const redisService = require("../services/redis.service");
+const queries = require("../queries/authQueries");
+const { URL_CHUNK } = require("../services/settings");
+const bcrypt = require("bcrypt");
+const { redirect } = require("./authController");
 
 module.exports = {
   redirectUri: (req, res) => {
-    const {app} = req.query;
+    const { app } = req.query;
     let authUri;
-    if (req.path === '/google') authUri = googleAuthUri;
-    if (req.path === '/github') authUri = githubAuthUri;
-    if (app !== "")  console.log(app);
-    console.log('redirectUri', req.path);
-    res.cookie('appname', app);
-    console.log(res.appName);
+    if (req.path === "/google") authUri = googleAuthUri;
+    if (req.path === "/github") authUri = githubAuthUri;
+    // if (app !== "")  console.log(app);
+    // console.log('redirectUri', req.path);
+    res.cookie("appname", app);
+    // console.log(res.appName);
     res.redirect(authUri);
   },
   login: async (req, res, next) => {
-    req.body = {}
-    req.body.app = req.cookies['appname'];
-    const { email} = req.credentials;
-    const {userId} = await queries.getOneByEmail(email);
-    
+    req.body = {};
+    req.body.app = req.cookies["appname"];
+    const { email } = req.credentials;
+    const { userId } = await queries.getOneByEmail(email);
+
     if (!userId && !req.loginCount) {
       req.loginCount = true;
       return next();
@@ -33,36 +33,36 @@ module.exports = {
     // set JWT cookies http only
     const me = await queries.getMe(userId);
     // console.log(me);
-    const [accessToken, refreshToken] = jwtService.getTokens({...me, password: ''});
-    
-    res.cookie('access_token', accessToken, cookieService.options);
-    res.cookie('refresh_token', refreshToken, cookieService.options);
+    const [accessToken, refreshToken] = jwtService.getTokens({
+      ...me,
+      password: "",
+    });
+
+    res.cookie("access_token", accessToken, cookieService.options);
+    res.cookie("refresh_token", refreshToken, cookieService.options);
     // set id = loggued in redis
     redisService.setLogin(me.id);
     return redirect(req, res);
   },
   signup: async (req, res, next) => {
-    console.log('google signup');
-    let {
-      firstname, lastname, nickname, password, email
-    } = req.credentials;
+    console.log("google signup");
+    let { firstname, lastname, nickname, password, email } = req.credentials;
     // console.log(req.credentials);
     try {
-      const hash = await bcrypt.hash(password, 10)
+      const hash = await bcrypt.hash(password, 10);
       const newUserId = await queries.insertUser({
-        firstname, 
-        lastname, 
-        nickname, 
-        password: hash, 
+        firstname,
+        lastname,
+        nickname,
+        password: hash,
         email,
-        apisignup: true
+        apisignup: true,
       });
-      console.log('newUser', newUserId);
+      console.log("newUser", newUserId);
       return next();
-      
-    } catch(error) {
+    } catch (error) {
       console.log(error);
-      return res.json({message: error.toString()});
+      return res.json({ message: error.toString() });
     }
-  }
-}
+  },
+};
--- api/services/settings.js
+++ api/services/settings.js
@@ -1,11 +1,11 @@
 module.exports = {
-  BASE_URL : process.env.NODE_ENV === 'production' ?
-    "https://auth.ikodi.eu"
-  :
-    "http://localhost:5050",
-  FRONT_URL : process.env.NODE_ENV === 'production' ?
-    "https://auth.ikodi.eu"
-  :
-    "http://localhost:8001",
-  URL_CHUNK: process.env.NODE_ENV === 'production' ? 'https://' : 'http://',
-} 
+  BASE_URL:
+    process.env.NODE_ENV === "production"
+      ? "https://auth.ikodi.eu"
+      : "http://localhost:5050",
+  FRONT_URL:
+    process.env.NODE_ENV === "production"
+      ? "https://auth.ikodi.eu"
+      : "http://localhost:8080",
+  URL_CHUNK: process.env.NODE_ENV === "production" ? "https://" : "http://",
+};
