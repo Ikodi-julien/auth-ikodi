afc1a4e151caa59fc79959da124390bfb19fb95a
ikodi-julien <jupellin39@gmail.com>
2022-01-06 10:20:47 +0100

    fix background css in login-success.ejs + fix email in req.body from apiLogin, atempt n2

--- api/controllers/apiLoginController.js
+++ api/controllers/apiLoginController.js
@@ -24,6 +24,7 @@ module.exports = {
     req.body = {};
     req.body.app = req.cookies["appname"];
     const { email } = req.credentials;
+    req.body.email = email;
     const { userId } = await queries.getOneByEmail(email);
 
     if (!userId && !req.loginCount) {
