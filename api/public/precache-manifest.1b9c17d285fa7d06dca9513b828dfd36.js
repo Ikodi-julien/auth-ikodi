8beb1519baa4d724984349c0b65a41ceb6443303
ikodi-julien <jupellin39@gmail.com>
2022-05-10 21:46:50 +0200

    New Build

---
+++ api/public/precache-manifest.1b9c17d285fa7d06dca9513b828dfd36.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f97e012fe2a48168c0",
+    "url": "/css/app.ae46390e.css"
+  },
+  {
+    "revision": "15a4a1836b7425a6b1103436518ee08e",
+    "url": "/img/blog_20210104.15a4a183.png"
+  },
+  {
+    "revision": "fadfbd682a0039aeb3f5a8939a9865c5",
+    "url": "/img/chronofit-v2.fadfbd68.png"
+  },
+  {
+    "revision": "7c665a6858afd6f40cd5d11f380a1fcd",
+    "url": "/img/concord-portefolio.7c665a68.png"
+  },
+  {
+    "revision": "61bd20166f5fdedaaadb3f257a853ed4",
+    "url": "/img/feedback_portfolio.61bd2016.png"
+  },
+  {
+    "revision": "600b61aa67d415020858a94275980ffe",
+    "url": "/img/games_medley.600b61aa.png"
+  },
+  {
+    "revision": "a69758c0da96d52c33847549d3ed9b6b",
+    "url": "/img/multiplicorne_portfolio.a69758c0.png"
+  },
+  {
+    "revision": "da9c0b79a94258fad21c74eff7849e2e",
+    "url": "/img/portfolio_portfolio.da9c0b79.png"
+  },
+  {
+    "revision": "6c88135f8a1a4e3a4c5af818c7384144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f97e012fe2a48168c0",
+    "url": "/js/app.5ad561a6.js"
+  },
+  {
+    "revision": "a1c24e5308f43cc5c6ad",
+    "url": "/js/chunk-vendors.7a558330.js"
+  },
+  {
+    "revision": "898e8564852e65bd90a74e0c2568c4b4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "685a574cb7b4b1eb749fd5327422a9fb",
+    "url": "/site.webmanifest"
+  }
+]);
