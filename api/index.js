b117ff7e580a5ff724030e2606d1274c667e2aee
ikodi-julien <jupellin39@gmail.com>
2023-08-27 19:36:15 +0200

    Modif pour fonctionnement avec MySql

--- api/index.js
+++ api/index.js
@@ -1,32 +1,29 @@
-require("dotenv").config();
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const router = require("./routes/router");
-const chronofitRouter = require("./routes/chronofitRouter");
-const PORT = process.env.PORT;
+require('dotenv').config()
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const router = require('./routes/router')
+const chronofitRouter = require('./routes/chronofitRouter')
+const PORT = process.env.PORT
 // const FRONT_PORT = process.env.FRONT_PORT;
 // const APP_PORT = process.env.APP_PORT;
 
 const corsOptions = {
-  origin:
-    process.env.NODE_ENV === "production"
-      ? [/\.pellin.eu\/?$/]
-      : [/^http:\/\/localhost\:\d{4}$/],
-  credentials: true,
-};
+    origin: [/\.pellin.eu\/?$/, /^http:\/\/localhost\:\d{4}$/],
+    credentials: true,
+}
 
-const app = express();
-app.use(cors(corsOptions));
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+const app = express()
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.json());
-app.set("views", "./public/views");
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(router, chronofitRouter);
+app.use(express.json())
+app.set('views', './public/views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(router, chronofitRouter)
 
 app.listen(process.env.PORT, () => {
-  console.log(`Started on http://localhost:${PORT}`);
-});
+    console.log(`Started on http://localhost:${PORT}`)
+})
--- api/queries/chronofitQueries.js
+++ api/queries/chronofitQueries.js
@@ -1,65 +1,70 @@
-const db = require("../database/psqlDB");
+// const db = require("../database/psqlDB");
+const { myQuery, myQueryReturnInsertId } = require('../database/mysqlDB')
 
 module.exports = {
-  getTrainings: async (id) => {
-    const queryString = `SELECT id, user_id, name, type, duration, timecap, exos, created_at, to_char(created_at, 'DD TMmon YYYY, HH24hMI') as date FROM auth.trainings 
-    WHERE user_id=$1
-    ORDER BY created_at DESC`;
+    getTrainings: async (id) => {
+        const queryString = `SELECT id, user_id, name, type, duration, timecap, exos, created_at, date_format(created_at, '%d/%M/%Y, %H:%i') as date FROM chronofit.trainings 
+    WHERE user_id= ?
+    ORDER BY created_at DESC`
 
-    const result = await db.query(queryString, [id]);
-    // console.log("getTrainings", result);
-    return result.rows;
-  },
-  /**
-   * Insert new training in table trainings
-   * @returns {Object} training_id
-   */
-  insertTraining: async (data) => {
-    const { userId, name, type, duration, timecap, exos } = data;
+        const results = await myQuery(queryString, [id])
+        // console.log("getTrainings", result);
+        return results
+    },
+    /**
+     * Insert new training in table trainings
+     * @returns {Object} training_id
+     */
+    insertTraining: async (data) => {
+        const { userId, name, type, duration, timecap, exos } = data
 
-    const queryString = `INSERT INTO auth.trainings ("user_id", "name", "type", "duration", "timecap", "exos") VALUES ($1,$2,$3,$4,$5,$6) RETURNING id, user_id, name, type, duration, timecap, exos, created_at`;
+        const queryString = `INSERT INTO chronofit.trainings (user_id, name, type, duration, timecap, exos) VALUES (?,?,?,?,?,?) `
 
-    const result = await db.query(queryString, [
-      userId,
-      name,
-      type,
-      duration,
-      timecap,
-      exos,
-    ]);
-    return result.rows[0];
-  },
-  deleteTraining: async (data) => {
-    const { id, userId } = data;
-    // console.log("deleteTraining : ", id, userId);
-    const queryString = "DELETE FROM auth.trainings WHERE id=$1 and user_id=$2";
+        const insertId = await myQueryReturnInsertId(queryString, [
+            userId,
+            name,
+            type,
+            duration,
+            timecap,
+            JSON.stringify(exos),
+        ])
 
-    try {
-      const result = await db.query(queryString, [id, userId]);
-      return result.rowCount ? true : false;
-    } catch (error) {
-      return error;
-    }
-  },
-  updateTraining: async (data) => {
-    const { id, userId, training } = data;
+        const queryString_2 = `SELECT * FROM chronofit.trainings WHERE id = ?`
+        const result = await myQuery(queryString_2, [insertId])
+        return result
+    },
+    deleteTraining: async (data) => {
+        const { id, userId } = data
+        // console.log("deleteTraining : ", id, userId);
+        const queryString =
+            'DELETE FROM chronofit.trainings WHERE id= ? and user_id= ?'
 
-    const queryString = `UPDATE auth.trainings 
+        try {
+            const result = await myQuery(queryString, [id, userId])
+            return result.rowCount ? true : false
+        } catch (error) {
+            return error
+        }
+    },
+    updateTraining: async (data) => {
+        const { id, userId, training } = data
+
+        const queryString = `UPDATE chronofit.trainings 
     SET "name"=$1, "type"=$2, "duration"=$3, "timecap"=$4, "exos"=$5 
-    WHERE auth.trainings.id = $6
-    AND auth.trainings.user_id = $7
-    RETURNING id`;
+    WHERE chronofit.trainings.id = $6
+    AND chronofit.trainings.user_id = $7
+    RETURNING id`
 
-    const result = await db.query(queryString, [
-      training.name,
-      training.type,
-      training.duration,
-      training.timecap,
-      training.exos,
-      id,
-      userId,
-    ]);
-    console.log("updateTraining", result);
-    return result.rows[0];
-  },
-};
+        const result = await db.query(queryString, [
+            training.name,
+            training.type,
+            training.duration,
+            training.timecap,
+            training.exos,
+            id,
+            userId,
+        ])
+        console.log('updateTraining', result)
+        return result.rows[0]
+    },
+}
